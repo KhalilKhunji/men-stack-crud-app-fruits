404a6adce8507862ff88d65b56975a63ae9d56e2
KhalilKhunji <khalilkhunji@gmail.com>
2024-07-21 12:08:04 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('The server is running!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
