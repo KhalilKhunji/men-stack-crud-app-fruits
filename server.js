5057386eef636895eebb4a48d652fe3ec783dc86
KhalilKhunji <khalilkhunji@gmail.com>
2024-07-21 12:19:32 +0300

    isetup express

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// Import modules
 const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+
+// Middleware
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('The server is running!');
+app.get("/", async (req, res) => {
+    res.send("hello, friend!");
 });
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
